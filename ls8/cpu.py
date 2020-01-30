e848bc63fc33f37ed5bd04e05198a2504bc4d650
Dora Belme <contact.dorabelme@gmail.com>
2020-01-29 22:57:57 -0500

    MVP Day 4

--- ls8/cpu.py
+++ ls8/cpu.py
@@ -15,7 +15,7 @@ class CPU:
         # PC
         self.pc = 0
         # stack pointer (SP)
-        self.sp = 255
+        self.sp = 256
 
         # self.mdr = 0
         # Commands
@@ -27,7 +27,8 @@ class CPU:
             0b01000101: self.push,
             0b01000110: self.pop,
             0b01010000: self.call,
-            0b00010011: self.iret
+            0b00010001: self.iret,
+            0b10100000: self.add
         }
 
     # accepts the address in RAM and returns the value stored there
@@ -58,35 +59,50 @@ class CPU:
         self.alu("MUL", operand_a, operand_b)
         return (3, True)
 
+    def add(self, operand_a, operand_b):
+        # calling alu function
+        self.alu("ADD", operand_a, operand_b)
+        return (3, True)
+
     def push(self, operand_a, operand_b):
-        reg_address = self.ram[self.pc + 1]
+        # reg_address = self.ram[self.pc + 1]
         self.sp -= 1
-        value = self.reg[reg_address]
+        value = self.reg[operand_a]
         self.ram[self.sp] = value
         return (2, True)
 
     def pop(self, operand_a, operand_b):
         pop_value = self.ram[self.sp]
-        reg_address = self.ram[self.pc + 1]
+        reg_address = operand_a
         self.reg[reg_address] = pop_value
         self.sp += 1
         return (2, True)
 
-    def call(self, operand_a, *args):
+    def call(self, operand_a, operand_b):
+        # push the next program counter on the stack
+        next_address = self.pc + 2
+        self.reg[2] = next_address
+        self.push(2, None)
 
-        self.sp -= 1
-        self.ram_write(self.sp, self.pc)
-        self.push(operand_a)
-        self.pc = self.mdr - 2
+        # move program counter to the start of first command of the function
+        routine_address = self.reg[operand_a]
+        # print(operand_a, routine_address)
+        self.pc = routine_address
+
+        return (0, True)
 
     def iret(self, *args):
-        """
-        Pop the value from the top of the stack and store it in the PC
-        """
-        self.pop(self.mdr)
-        self.pc = self.ram[self.sp] + 1
+        self.pop(2, None)
+        next_address = self.reg[2]
+        self.pc = next_address
+
+        # next_address = self.ram[self.sp]
+        # self.sp += 1
+        # self.pc = next_address
+        return (0, True)
 
     # loading from a file
+
     def load(self, program):
         """Load a program into memory."""
 
@@ -113,12 +129,18 @@ class CPU:
                 comment_split = line.split('#')
                 num = comment_split[0].strip()
 
+                if num == '':
+                    continue
+
                 try:
+                    # print(num)
                     self.ram_write(int(num, 2), address)
                     address += 1
                 except ValueError:
-                    print("Value error")
+                    # print("Value error")
                     pass
+
+        # print("Finished reading the program")
 
     def alu(self, op, reg_a, reg_b):
         """ALU operations."""
@@ -162,11 +184,12 @@ class CPU:
             operand_a = self.ram_read(self.pc + 1)
             operand_b = self.ram_read(self.pc + 2)
 
+            # print(instruction_register, operand_a, operand_b)
+
             try:
                 f = self.commands[instruction_register]
                 # print(f)
-                operation_op = f(operand_a, operand_b
-                                 )
+                operation_op = f(operand_a, operand_b)
                 running = operation_op[1]
                 self.pc += operation_op[0]
 
